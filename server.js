227ea11a5ff605dc047c8201aa2467a51073f321
bagaskaramadhan <bagaskaramadhan@gmail.com>
2021-01-12 22:40:02 +0700

    test 1

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const server = express()
 const { PORT } = require('./src/helpers/env')
+const router = require('./src/routes/route')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const router = require('./src/routes/route')
 
 server.use(cors())
 server.use(bodyParser.urlencoded({ extended: false }))
